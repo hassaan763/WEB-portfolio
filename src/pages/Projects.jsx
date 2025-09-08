e1552f50dff2db22a70a4153be4579c32250265f
Hassaan Ahmad <jakesam786@gmail.com>
2025-09-08 15:31:32 +0500

    Initial commit - portfolio

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,18 @@
+import ProjectsGrid from '../components/projects/ProjectsGrid';
+import { ProjectsProvider } from '../context/ProjectsContext';
+import { useEffect } from 'react';
+
+const Projects = () => {
+    useEffect(() => {
+        document.title = 'HASSAAN — Projects';
+    }, []);
+	return (
+		<ProjectsProvider>
+			<div className="container mx-auto">
+				<ProjectsGrid />
+			</div>
+		</ProjectsProvider>
+	);
+};
+
+export default Projects;
